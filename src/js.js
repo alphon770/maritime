e0ada391c6c6e189671b5434c5ecf93a9b8ed961
alphon770 <ayoub770@outlook.fr>
2021-12-23 17:13:38 +0000

    i like that

---
+++ src/js.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+    {
+    loop: true,
+    items: 1,
+    autoplay:true,
+    autoplayTimeout:4000,
+    animateOut: 'fadeOut'
+
+    });
+
+});
