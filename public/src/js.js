ff62fe52764f2767ed9041fbc7397db1ff4e29de
alphon770 <ayoub770@outlook.fr>
2021-12-23 23:09:40 +0000

    heroku

---
+++ public/src/js.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+    {
+    loop: true,
+    items: 1,
+    autoplay:true,
+    autoplayTimeout:4000,
+    animateOut: 'fadeOut'
+
+    });
+
+});
+
+
+    $('body').hide();
+
+
+$(document).ready(function() {
+    $('body').show();
+});
